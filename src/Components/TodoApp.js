bbc89db6152042cbf41f63547e8745cf4a2d63eb
HansMoura20 <hansadrienmoura@outlook.com>
2020-12-02 09:54:13 +0800

    adding styles for the Todolist component

--- src/Components/TodoApp.js
+++ src/Components/TodoApp.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
-import TodoList from './TodoList';
+
 
 const TodoApp = () => {
   //This is some default values arrays for the useState
